731a1def200fb18b67f804cc7c63da07434b0ef1
Renard Gold <goldrenard@gmail.com>
2017-08-04 21:25:43 +0400

    Initial commit

---
+++ src/test/java/ru/caramel/juniperbot/JuniperbotApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.caramel.juniperbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JuniperbotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
