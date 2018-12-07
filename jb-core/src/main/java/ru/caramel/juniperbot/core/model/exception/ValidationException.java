b8b63184af28e17859cc8e0fa1e0052ed0e5b7bc
Caramel <goldrenard@gmail.com>
2018-12-07 21:24:38 +0400

    optimize validation exceptions

--- jb-core/src/main/java/ru/caramel/juniperbot/core/model/exception/ValidationException.java
+++ jb-core/src/main/java/ru/caramel/juniperbot/core/model/exception/ValidationException.java
@@ -21,10 +21,6 @@ public class ValidationException extends DiscordException {
     private static final long serialVersionUID = 2474643174485112049L;
 
     public ValidationException(String message, Object... args) {
-        super(message, args);
-    }
-
-    public ValidationException(String message, Throwable cause, Object... args) {
-        super(message, cause, args);
+        super(message, null, false, false, args);
     }
 }
